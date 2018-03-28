781914180d67cd1004da77dc709b4494f35c0601
Tiosneike <31991303+Tiosneike@users.noreply.github.com>
2018-03-28 01:39:55 -0300

    Add files via upload

--- funcao.php
+++ funcao.php
@@ -1,31 +1,62 @@
 <?php
-
 require_once 'formulario.html';
 
-  function Calcular() {
-
-
-    if (isset($_POST['doCalc'])) {
-
-
-        if ($_POST['operacao'] == "soma") {
-
-
-            $resultado = $_POST['v1'] + $_POST['v2'];
-             return $resultado;
-
-            
-        } elseif ($_POST['operacao'] == "subtrai") {
-            $resultado = $_POST['v1'] - $_POST['v2'];
-            return $resultado;
-        } elseif ($_POST['operacao'] == 'multiplica') {
-            $resultado = $_POST['v1'] * $_POST['v2'];
-            return $resultado;
-        } elseif ($_POST['operacao'] == 'divide') {
-            $resultado = $_POST['v1'] / $_POST['v2'];
-            return $resultado;
-        }
-    }
+if ($_POST){
+    $v1 = ($_POST) ["v1"];
+    $v2 = ($_POST) ["v2"];
+    $op = $_POST ["operacao"];
 }
-echo Calcular();
+
+    function validar ($v1,$v2){
+        return is_numeric($v1)&&is_numeric($v2);
+    }
+    
+    function somar($v1,$v2){
+      if(validar($v1,$v2))  return $v1 + $v2;
+      else echo "O valor Inserido é Invalido"   ;
+    }
+
+    function subtrair($v1,$v2){
+      if(validar($v1,$v2))  return $v1 - $v2;
+      else echo "O valor Inserido é Invalido";
+    }
+
+    function dividir($v1,$v2){
+      if(validar($v1,$v2))  return $v1 / $v2;
+      else echo "O valor Inserido é Invalido";
+    }
+  
+    function multiplicar ($v1,$v2){
+      if(validar($v1,$v2))  return $v1 * $v2;
+      else echo "O valor Inserido é Invalido";
+    }
+
+      if (isset($_POST['doCalc'])) {
+
+        if ($_POST['operacao'] == "somar") {
+        
+        $resultado = somar( $_POST['v1'] , $_POST['v2']);
+        
+        echo $resultado;
+        
+        } elseif ($_POST['operacao'] == "subtrair") {
+        
+        $resultado = subtrair($_POST['v1'] , $_POST['v2']);
+        
+        echo $resultado;
+        
+        } elseif ($_POST['operacao'] == 'multiplicar') {
+        
+        $resultado = multiplicar( $_POST['v1'] , $_POST['v2']);
+        
+        echo $resultado;
+        
+        } elseif ($_POST['operacao'] == 'dividir') {
+        
+        $resultado = dividir($_POST['v1'] , $_POST['v2']);
+        
+        echo $resultado;
+        
+        }
+    }   
 ?>
